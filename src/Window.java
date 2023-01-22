3a77a059beffa8e8bd9eafc08550e9c54f62b90d
Martimf95 <martims766@gmail.com>
2023-01-22 15:58:28 +0100

    Merge remote-tracking branch '1.1/master'

--- src/Window.java
+++ src/Window.java
@@ -8,6 +8,7 @@ public class Window extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new GridLayout(4,3,10,10));
 
+        // created the buttons
         this.add(new JButton("1"));
         this.add(new JButton("2"));
         this.add(new JButton("3"));
