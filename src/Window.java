b3120aabe34a87f5dcac6d730e7302302e2cb80f
Martimf95 <martims766@gmail.com>
2023-01-23 18:22:59 +0100

    Merge remote-tracking branch '1.1/master'

--- src/Window.java
+++ src/Window.java
@@ -6,22 +6,38 @@ public class Window extends JFrame{
 
     public Window(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(4,3,10,10));
+        this.setLayout(new GridLayout(2,1,20,20));
 
-        ImageIcon ImageIcon = new ImageIcon("taschenrechner.jpg");
-        this.setIconImage(new ImageIcon(ImageIcon.getImage()).getImage());
+        ImageIcon image = new ImageIcon("C:\\Users\\marvi\\Desktop\\Programmieren\\Java\\A_Calculator_with _Java\\src\\taschenrechner.jpg");
+        this.setIconImage(image.getImage());
+
+        TextField textfield = new TextField("Zahlen",170);
+        this.add(textfield);
+
+        JPanel textfeldpanel = new JPanel();
+
+        textfeldpanel.add(textfield);
+
+        JPanel tasten = new JPanel();
+        tasten.setLayout(new GridLayout(3,3,10,10));
 
         // created the buttons
-        this.add(new JButton("1"));
-        this.add(new JButton("2"));
-        this.add(new JButton("3"));
-        this.add(new JButton("4"));
-        this.add(new JButton("5"));
-        this.add(new JButton("6"));
-        this.add(new JButton("7"));
-        this.add(new JButton("8"));
-        this.add(new JButton("9"));
+        tasten.add(new JButton("1"));
+        tasten.add(new JButton("2"));
+        tasten.add(new JButton("3"));
+        tasten.add(new Button("Del"));
+        tasten.add(new JButton("4"));
+        tasten.add(new JButton("5"));
+        tasten.add(new JButton("6"));
+        tasten.add(new Button("*"));
+        tasten.add(new JButton("7"));
+        tasten.add(new JButton("8"));
+        tasten.add(new JButton("9"));
+        tasten.add(new Button("+"));
 
+
+        this.add(textfeldpanel);
+        this.add(tasten);
         this.pack();
         this.setVisible(true);
     }
